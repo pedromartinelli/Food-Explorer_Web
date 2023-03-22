0d05210a0ef5dad356af06ff6e75a5cf0708c249
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-03-22 15:32:26 -0300

    configurando o zod na tela de Sign Up

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,16 +1,39 @@
+import * as zod from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 import { Envelope } from 'phosphor-react'
 import logoImage from '../../assets/logo.svg'
 
 import { Link } from 'react-router-dom'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { FcGoogle } from 'react-icons/fc'
-
+import { RiErrorWarningFill } from 'react-icons/ri';
 
 import { Container, FormDiv, Input, Form, InputContainer } from './styles'
 import { Button } from '../../components/Button'
 import { NavButton } from '../../components/NavButton'
 
 export function SignUp() {
+  const schema = zod.object({
+    name: zod.string().min(1, { message: 'Introduza um nome.' }),
+    email: zod.string().email({ message: 'Introduza um endereço de email válido.' }),
+    password: zod.string().min(8, { message: 'A senha deve ter no mínimo 8 caracteres.' })
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: zodResolver(schema)
+  });
+
+  function handleSignUp(user: any) {
+    console.log(user)
+  };
+
+
   return (
     <Container>
       <div className='flex md:gap-x-20 lg:gap-x-52 xl:gap-x-80'>
@@ -28,47 +51,54 @@ export function SignUp() {
             Cadastro
           </h1>
 
-          <Form>
+          <Form
+            onSubmit={handleSubmit((d) => handleSignUp(d))}
+          >
 
-            <InputContainer>
+            <div className='flex items-center'>
               <FiUser
                 className='absolute ml-4'
                 color='#7C7C8A'
                 size={22}
               />
               <Input
-                type='text'
-                name='name'
                 placeholder='Nome'
+                type='text'
+                {...register('name')}
               />
-            </InputContainer>
+            </div>
+            {errors.name?.message && <p className='text-xs -mt-6 mx-2 text-[#C0433D] flex items-center gap-2'> <RiErrorWarningFill /> {errors.name?.message}</p>}
 
-            <InputContainer>
+            <div className='flex items-center'>
               <FiMail
                 className='absolute ml-4'
                 color='#7C7C8A'
                 size={22}
               />
               <Input
+                placeholder='E-mail'
                 type='text'
-                name='email'
-                placeholder='Email'
+                {...register('email')}
               />
-            </InputContainer>
+            </div>
+            {errors.email?.message && <p className='text-xs -mt-6 mx-2 text-[#C0433D] flex items-center gap-2'> <RiErrorWarningFill /> {errors.email?.message}</p>}
 
-            <InputContainer>
+            <div className='flex items-center'>
               <FiLock
                 className='absolute ml-4'
                 color='#7C7C8A'
                 size={22} />
               <Input
-                type='password'
-                name='password'
                 placeholder='Senha'
+                type='password'
+                {...register('password')}
               />
-            </InputContainer>
+            </div>
+            {errors.password?.message && <p className='text-xs -mt-6 mx-2 text-[#C0433D] flex items-center gap-2'> <RiErrorWarningFill /> {errors.password?.message}</p>}
 
-            <Button title='Entrar' $large={true} />
+            <Button
+              title='Criar conta'
+            />
           </Form>
 
           <div className='mt-12 mb-6 grid grid-cols-3 items-center w-full'>
@@ -82,7 +112,7 @@ export function SignUp() {
               Se você já tem <br className='md:hidden' /> uma conta, <br className='sm:hidden md:flex' /> vá para o login.
             </p>
 
-            <NavButton title='Voltar para o login' to='/'/>
+            <NavButton title='Voltar para o login' to='/' />
           </div>
         </FormDiv>
       </div>
